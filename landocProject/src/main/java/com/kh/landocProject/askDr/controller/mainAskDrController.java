586bbd5a2316e83ceb439913251558d0290a8dc3
kobeomseok <kobeomseok@DESKTOP-O5DQE4O>
2020-07-15 00:42:04 +0900

    200715_00:41_마스터 askDr, product부분 연동완료

--- landocProject/src/main/java/com/kh/landocProject/askDr/controller/mainAskDrController.java
+++ landocProject/src/main/java/com/kh/landocProject/askDr/controller/mainAskDrController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class mainAskDrController {
 	@RequestMapping(value="askDr.do", method=RequestMethod.GET)
 	public String askDr() {
-		return "askDr/askDr";
+		return "askDr/askDr";		//askDr, product 메인 연동해놓음 200715_00:41_범석
 	}
 }
