1d22c4e1ca61906e4bdd07813368e6a8f437482e
오진교 <@LAPTOP-93CPOJLC>
2020-07-17 15:10:16 +0900

    2020/07/17_15:10_오진교_로그인

--- landocProject/src/main/java/com/kh/landocProject/member/model/service/MainMemberService.java
+++ landocProject/src/main/java/com/kh/landocProject/member/model/service/MainMemberService.java
@@ -1,10 +1,15 @@
 package com.kh.landocProject.member.model.service;
 
 import com.kh.landocProject.member.model.vo.Client;
+import com.kh.landocProject.member.model.vo.DrClient;
 
 public interface MainMemberService {
 
 	int joinClient(Client c);
+
+	Client loginClient(Client c);
+
+	DrClient loginDoctor(DrClient d);
 
 	
 
